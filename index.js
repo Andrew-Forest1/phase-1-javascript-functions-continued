3e2601bbf2a572d60d670d2979b59df01e9c7490
Andrew <aforest41@yahoo.com>
2022-11-23 09:26:18 -0800

    commit tree c944423e9aa50c165248f66f3e219cf5dbc25092

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+const array = (function (thingToAdd) {
+    const base = 3;
+    return [
+      function () {
+        return base + thingToAdd;
+      },
+      function () {
+        return base;
+      },
+    ];
+  })(2);
+
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = (function (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+});
+
+function wrapAdjective(s = "*"){
+    const statement = (function(adjective = 'special'){return `You are ${s}${adjective}${s}!`});
+    return statement;
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
